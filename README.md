1dab28553f349537a738ce38324ae91f61e93694
Tobermory <mydesireiscoma@outlook.com>
2018-01-22 17:52:23 +1000

    Add simple nodejs server

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# alphabet-learning-files
+# Alphabet learning files
 Files (alphabets) for applications based on alphabet learning tool
---
+++ server.js
@@ -0,0 +1,61 @@
+const http = require('http')
+const path = require('path')
+const url = require('url')
+const fs = require('fs')
+
+const availableLanguages = require('./alphabets/index.json')
+
+const server = new http.Server((request, response) => {
+  const requestUrl = url.parse(request.url, true).pathname
+  const requestQuery = url.parse(request.url, true).query
+
+  if (requestUrl === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/json' })
+    response.write(JSON.stringify(availableLanguages))
+  } else if (requestUrl === '/language') {
+    const queryLanguage = requestQuery.language
+    const queryAlphabet = requestQuery.alphabet
+
+    if (
+      !/^([a-z]{0,50}$)/.test(queryLanguage) ||
+      !/^([a-z]{0,50}$)/.test(queryAlphabet)
+    ) {
+      response.writeHead(404)
+      response.end()
+      return
+    }
+
+    const language = availableLanguages.find((language) => {
+      return language.name === queryLanguage
+    })
+
+    const alphabet = language ? language.alphabets.find((alphabet) => {
+      return alphabet.name === queryAlphabet
+    }) : null
+
+    if (language && alphabet) {
+      const filePath = path.join(
+        __dirname,
+        'alphabets',
+        language.name,
+        alphabet.file
+      )
+
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          response.writeHead(404)
+          response.end()
+        } else {
+          response.writeHead(200, 'Content-type', 'application/zip')
+          response.end(data)
+        }
+      })
+    } else {
+      response.writeHead(404)
+      response.end()
+    }
+  } else {
+    response.writeHead(404)
+    response.end()
+  }
+})
